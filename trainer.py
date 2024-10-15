b6fa64164d6930f0d2eb50ddbb8741cfb25aa40c
hashpad <37782032+hashpad@users.noreply.github.com>
2024-10-15 02:10:30 +0200

    Clean up, add README

--- trainer.py
+++ trainer.py
@@ -5,6 +5,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.dummy import DummyClassifier
 
+import numpy as np
 
 
 
@@ -18,7 +19,7 @@ class Trainer:
             'dummy_mf': DummyClassifier(strategy="most_frequent"),
             'dummy_un': DummyClassifier(strategy="uniform"),
             'random_forest': RandomForestClassifier(),
-            'svm': SVC(),
+            'svm': SVC(probability=True),
             'knn': KNeighborsClassifier(),
             'mlp': MLPClassifier(max_iter=1000),
         }
@@ -79,5 +80,89 @@ class Trainer:
         precision_micro = precision_score(y_true, y_pred, average='micro')
         return accuracy, f1_weighted, recall_weighted, precision_weighted, f1_macro, recall_macro, precision_macro, f1_micro, recall_micro, precision_micro,
 
+    
+    # def precision_at_k(self, y_true, y_pred, k=2):
+    #     if len(y_pred)>k:
+    #         y_pred = y_pred[:k]
+
+    #     score = 0.0
+    #     num_hits = 0.0
+
+    #     for i,p in enumerate(y_pred):
+    #         if p in y_true and p not in y_pred[:i]:
+    #             num_hits += 1.0
+    #             score += num_hits / (i+1.0)
+
+    #     if not y_true:
+    #         return 0.0
+
+    #     return score / min(len(y_true), k)
+
+    # def precision_at_k(self, y_true, y_pred, k=2):
+    #     total_precision = 0
+    #     num_samples = len(y_true)
+        
+    #     for true_labels, pred_labels in zip(y_true, y_pred):
+    #         top_k_pred = pred_labels[:k]
+            
+    #         relevant_in_top_k = len(set(top_k_pred) & set(true_labels))
+            
+    #         tp = relevant_in_top_k
+    #         fp = len([x for x in top_k_pred if x not in set(true_labels)])
+    #         if tp + fp == 0:
+    #             precision = 0
+    #         else:
+    #             precision = tp / (tp + fp)
+    #         total_precision += precision
+    
+    #     # avg
+    #     precision_at_k = total_precision / num_samples
+    #     return precision_at_k
+    def precision_at_k(self, y_true, y_pred, k=2):
+        total_precision = 0
+        num_samples = len(y_true)
+        
+        for true_labels, pred_labels in zip(y_true, y_pred):
+            top_k_pred = pred_labels[:k]
+            
+            relevant_in_top_k = len(set(top_k_pred) & set(true_labels))
+            
+            precision = relevant_in_top_k / k
+            total_precision += precision
+    
+        # avg
+        precision_at_k = total_precision / num_samples
+        return precision_at_k
+
+    def recall_at_k(self, y_true, y_pred, k):
+        total_recall = 0
+        num_samples = len(y_true)
+        
+        for true_labels, pred_labels in zip(y_true, y_pred):
+            top_k_pred = pred_labels[:k]
+            
+            relevant_in_top_k = len(set(top_k_pred) & set(true_labels))
+            
+            recall = relevant_in_top_k / len(true_labels) if len(true_labels) > 0 else 0
+            total_recall += recall
+        
+        # avg
+        recall_at_k = total_recall / num_samples
+        return recall_at_k
+
+
+    def f1_at_k(self, y_true, y_pred, k):
+        precision = self.precision_at_k(y_true, y_pred, k)
+        recall = self.recall_at_k(y_true, y_pred, k)
+        
+        if precision + recall == 0:
+            return 0.0
+        
+        f1_at_k = 2 * (precision * recall) / (precision + recall)
+        return f1_at_k
+
+
     def get_best_params(self):
         return self.best_params
+    
+    
